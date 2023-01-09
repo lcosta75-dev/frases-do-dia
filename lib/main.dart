24a2078e649304f80513daeb48004ec186bb3b95
Lais Costa <lcosta.developer@gmail.com>
2023-01-09 16:18:01 -0300

    commit final

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MaterialApp(
@@ -15,6 +16,23 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  var _frases = [
+    "Sou apenas um pequeno planeta que se perde diariamente em todo o seu universo.",
+    "Novas amizades serão sempre bem-vindas para darem cor e alegria ao meu dia a dia.",
+    "Gratidão não é pagamento, mas um reconhecimento que se demonstra no dia a dia.",
+    "Nem toda mudança importante acontece de repente e faz barulho, algumas são silenciosas e vão se fazendo no dia a dia."
+  ];
+
+  var _fraseGerada = "Clique abaixo para gerar uma frase!";
+
+  void _gerarFrase() {
+    var numeroSorteado = Random().nextInt(_frases.length);
+
+    setState(() {
+      _fraseGerada = _frases[numeroSorteado];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,31 +40,39 @@ class _HomeState extends State<Home> {
         title: Text("Frases do dia"),
         backgroundColor: Colors.green,
       ),
-      body: Container(
+      body: Center(
+          child: Container(
         padding: EdgeInsets.all(16),
-        decoration:
+        // width: double.infinity,
+        /*decoration:
             BoxDecoration(border: Border.all(width: 3, color: Colors.amber)),
-        child: Column(children: <Widget>[
-          Image.asset("images/logo.png"),
-          Text(
-            "Clique abaixo para gerar uma frase!",
-            textAlign: TextAlign.justify,
-            style: TextStyle(
-                fontSize: 17, fontStyle: FontStyle.italic, color: Colors.black),
-          ),
-          ElevatedButton(
-            child: Text(
-              "Nova frase",
-              style: TextStyle(
-                  fontSize: 25,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold),
-            ),
-            style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
-            onPressed: () {},
-          )
-        ]),
-      ),
+            */
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset("images/logo.png"),
+              Text(
+                _fraseGerada,
+                textAlign: TextAlign.justify,
+                style: TextStyle(
+                    fontSize: 25,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.black),
+              ),
+              ElevatedButton(
+                child: Text(
+                  "Nova frase",
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+                onPressed: _gerarFrase,
+              )
+            ]),
+      )),
     );
   }
 }
